a64a0255321edba74e98223bc502f543abab1687
Carlos <juancarlosgomezpacheco283@gmail.com>
2025-04-06 19:29:23 -0600

    Primer commit

---
+++ Indrustries/src/main/java/com/servicios/Indrustries/IndrustriesApplication.java
@@ -0,0 +1,13 @@
+package com.servicios.Indrustries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndrustriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndrustriesApplication.class, args);
+	}
+
+}
